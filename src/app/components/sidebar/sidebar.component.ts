e6439bee81ed98942279f73dcef785d65fd971d6
John W <81695802+john-wd@users.noreply.github.com>
2024-02-14 14:29:16 -0300

    sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.sass']
 })
 export class SidebarComponent implements OnInit {
 
