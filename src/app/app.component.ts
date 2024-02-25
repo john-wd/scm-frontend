580faf08dd937c27c133c2f6f115e01f4f9084eb
John W <81695802+john-wd@users.noreply.github.com>
2024-02-25 13:16:12 -0300

    standalone bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
+import { PlayerComponent } from './smashcustommusic/components/player/player.component';
+import { RouterOutlet } from '@angular/router';
+import { MainNavComponent } from './components/main-nav/main-nav.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.sass'],
+    standalone: true,
+    imports: [SidebarComponent, MainNavComponent, RouterOutlet, PlayerComponent]
 })
 export class AppComponent {
   title = 'smash-custom-music';
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,38 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { SmashcustommusicModule } from './app/smashcustommusic/smashcustommusic.module';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { FeatureFlagService } from './app/shared/services/feature-flag.service';
+import { APP_INITIALIZER, importProvidersFrom } from '@angular/core';
+
+import * as flagConfigs from "./config/production.flags.json";
+
+const featureFlagFactory = (featureFlagService: FeatureFlagService) => () => featureFlagService.loadConfig(flagConfigs)
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(BrowserModule, AppRoutingModule, SmashcustommusicModule, StoreModule.forRoot({}), EffectsModule.forRoot(), StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: false, // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+    })),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: featureFlagFactory,
+      deps: [FeatureFlagService],
+      multi: true
+    },
+    provideAnimations(),
+  ]
+})
   .catch(err => console.error(err));
