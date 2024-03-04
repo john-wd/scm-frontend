6abb8a54cdb2d99ed1f1c0c6d5f248de87c6f559
John W <81695802+john-wd@users.noreply.github.com>
2024-03-04 17:41:01 -0300

    apply changes from brstm player

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -49,7 +49,7 @@ export class PlayerService implements OnDestroy {
 
   configure(apiUrl: string) {
     this._apiUrl = apiUrl
-    this._player = new BrstmPlayer(this._apiUrl);
+    this._player = new BrstmPlayer();
   }
 
   constructor() {
@@ -91,7 +91,9 @@ export class PlayerService implements OnDestroy {
       this.playlistSubject.next(this.playlist)
       this.currentIndex = 0
     }
-    this._player.play(this.toInternalSong(song));
+
+    let url = this._apiUrl + "/" + song.song_id
+    this._player.play(url, this.toInternalSong(song));
   }
 
   playAtIndex(idx: number) {
