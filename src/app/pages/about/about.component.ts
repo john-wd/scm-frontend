e254769dcabaa2d88d33f1412642efbde493594c
John W <81695802+john-wd@users.noreply.github.com>
2024-03-27 14:07:53 -0300

    main about page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,11 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  styleUrls: ['./about.component.scss'],
+  standalone: true,
 })
 export class AboutComponent implements OnInit {
+  repoLink = "https://github.com/john-wd/scm-frontend"
+  telegramLink = "https://t.me/smashcustommusic"
+  mailLink = "mailto:contact@smashcustommusic.net"
 
   constructor() { }
 
