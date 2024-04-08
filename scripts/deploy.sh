040edae353b80512e34fc6968fc5eb2a9680c053
John W <81695802+john-wd@users.noreply.github.com>
2024-04-08 08:54:34 -0300

    troubleshoot gh action

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 S3_BUCKET="smashcustommusic-frontend"
 
@@ -21,8 +20,19 @@ if [ $? -gt 0 ]; then
   aws s3api put-bucket-policy --bucket $S3_BUCKET --policy file://${SCRIPT_DIR}/policy.json
 fi
 
+which ng > /dev/null
+if [ $? -gt 0 ]; then
+  # npm did not install angular, so do it
+  npm i
+fi
 ng build
+
+[ $? -gt 0 ] && exit 1
+
 aws s3 cp --recursive ./dist/smash-custom-music s3://$S3_BUCKET
+[ $? -gt 0 ] && exit 1
+
 aws s3 website s3://$S3_BUCKET --index-document index.html --error-document index.html
+[ $? -gt 0 ] && exit 1
 
 echo "Url: http://$S3_BUCKET.s3-website-$REGION.amazonaws.com"
